6e5eec6a76a5373f0060c815a887044ac022d2f5
Michael Schultz <43320327+SkylarZYX@users.noreply.github.com>
2023-08-29 16:05:15 -0400

    Update deadline.bat

--- deadline.bat
+++ deadline.bat
@@ -1,6 +1,4 @@
 @echo off
-Rem Drive letter your deadline-scheduler directory is at
-d: 
 Rem Change this to your deadline-scheduler directory
-cd D:\path\to\deadline-scheduler
+cd /D D:\path\to\deadline-scheduler
 start deadline.vbs
